a6b2ffb4b981f176cad75d7a1b60b0f772ebc3d7
Bünyamin Talha Bostan <btalhabostan8@gmail.com>
2023-12-20 02:09:18 +0100

    Nodes have added the map.

--- app/app.py
+++ app/app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import json
+
 app = Flask(__name__)
+
+
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/get-network-data')
+def get_network_data():
+    with open('E:\\tum_task\\data\Germany_Nobel.json', 'r') as file:
+        data = json.load(file)
+    return jsonify(data['networkStructure']['nodes']['node'])
+
+
+
 
 
 
